e8b952c98f384509f46f928efa07f231184665be
Mira Makadia <patelmirak@gmail.com>
2022-03-10 13:27:48 -0500

    adds authentication

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Borough(models.Model):
@@ -30,6 +31,13 @@ class Point_Of_Interest(models.Model):
         ])
     image = models.CharField(max_length = 300)
     description = models.TextField(max_length = 300)
-    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)        
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)  
+
+
+class Review(models.Model):
+    rating = models.IntegerField()
+    description = models.TextField(max_length = 300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 
