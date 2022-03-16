17e8cae273cf4e479bd05553aaa9c7091bb8851d
Max Ma <maxxma97@gmail.com>
2022-03-16 18:59:29 -0400

    wip: functionality for carousel

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -6,4 +6,21 @@
     totalItems = items.length,
     slide = 0,
     moving = true;
+
+    // set classes
+    function setInitialClasses() {
+        // targets prev, current, and next items - assumes there are at least 3 items
+        items[totalItems - 1].classList.add("prev");
+        items[0].classList.add("active");
+        items[1].classList.add("next");
+    }
+
+    // set event listeners
+    function setEventListeners() {
+        let next = document.getElementsByClassName("carousel-button--next")[0];
+        let prev = document.getElementsByClassName("carousel-button--prev")[0];
+
+        next.addEventListener("click", moveNext);
+        prev.addEventListener("click", movePrev);
+    }
 } (document));
