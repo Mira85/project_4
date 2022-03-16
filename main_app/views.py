ce759f6c1152277c707d71e38831c4058a2fb9bf
Manuel Martinez <m.martinez45@hotmail.com>
2022-03-16 12:35:59 -0400

    added additional fetches to neighborhood_fetch - nightclubs, lodging, clothing_store

--- main_app/views.py
+++ main_app/views.py
@@ -43,42 +43,73 @@ def neighborhood_detail(request, neighborhood_id):
     response = requests.get(tourist_attraction_url, headers=headers, data=payload)
     neighborhood_tourist_attractions = response.json()['results']
     result_list.append(neighborhood_tourist_attractions)
+
     # Restaurants
     restaurant_url = f"{NEIGHBORHOOD_BASE_URL}{neighborhood.name}&type=restaurant&key={API_KEY}"
     response = requests.get(restaurant_url, headers=headers, data=payload)
     neighborhood_restaurants = response.json()['results']
     result_list.append(neighborhood_restaurants)
+    
+    # Night Clubs
+    club_url = f"{NEIGHBORHOOD_BASE_URL}{neighborhood.name}&type=night_club&key={API_KEY}"
+    response = requests.get(club_url, headers=headers, data=payload)
+    neighborhood_clubs = response.json()['results']
+    result_list.append(neighborhood_clubs)
+
+    #Lodging
+    lodging_url = f"{NEIGHBORHOOD_BASE_URL}{neighborhood.name}&type=lodging&key={API_KEY}"
+    response = requests.get(lodging_url, headers=headers, data=payload)
+    neighborhood_lodging = response.json()['results']
+    result_list.append(neighborhood_lodging)
+
+    # Museum
+    # museum_url = f"{NEIGHBORHOOD_BASE_URL}{neighborhood.name}&type=museum&key={API_KEY}"
+    # response = requests.get(museum_url, headers=headers, data=payload)
+    # neighborhood_museums = response.json()['results']
+    # result_list.append(neighborhood_museums)
+
+    # Clothing_Store
+    clothing_store_url = f"{NEIGHBORHOOD_BASE_URL}{neighborhood.name}&type=clothing_store&key={API_KEY}"
+    response = requests.get(clothing_store_url, headers=headers, data=payload)
+    neighborhood_clothing_stores = response.json()['results']
+    result_list.append(neighborhood_clothing_stores)
+
 
     for genre in result_list:
         for result in genre:
-            places = {
-                "name": result['name'],
-                "address": result['formatted_address'],
-                "interest_category": result['types'],
-                "rating": result['rating'],
-                "id": result['place_id'],
-            }
-            place_list.append(places)
-
-    # for result in neighborhood_restaurants:
-    #     places = {
-    #         "name": result['name'],
-    #         "address": result['formatted_address'],
-    #         "interest_category": result['types'],
-    #         "rating": result['rating'],
-    #         "id": result['place_id'],
-    #     }
-    #     place_list.append(places)
-    
-    # for result in neighborhood_tourist_attractions:
-    #     places = {
-    #         "name": result['name'],
-    #         "address": result['formatted_address'],
-    #         "interest_category": result['types'],
-    #         "rating": result['rating'],
-    #         "id": result['place_id'],
-    #     }
-    #     place_list.append(places)
+            if ['photos'][0] in result:
+                print(result['photos'][0]['photo_reference'])
+            if ['photos'][0] in result:
+                place = {
+                    "name": result['name'],
+                    "address": result['formatted_address'],
+                    "interest_category": result['types'],
+                    "rating": result['rating'],
+                    "id": result['place_id'],
+                    'photo': result['photos'][0]['photo_reference']
+                }
+                place_list.append(place)
+            else:
+                place = {
+                    "name": result['name'],
+                    "address": result['formatted_address'],
+                    "interest_category": result['types'],
+                    "rating": result['rating'],
+                    "id": result['place_id'],
+                    'photo': "Has No Photo"
+                }
+                place_list.append(place)
+                
+            # else: 
+            #     place = {
+            #         "name": result['name'],
+            #         "address": result['formatted_address'],
+            #         "interest_category": result['types'],
+            #         "rating": result['rating'],
+            #         "id": result['place_id'],
+            #         "photo": 'HAS PHOTO'
+            #     }
+                # place_list.append(place)
 
     return render(request, 'neighborhood/neighborhood_fetch.html', {
         'neighborhood': neighborhood,
