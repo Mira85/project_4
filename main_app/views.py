eebafc23e870de5182154fc05e4a6c91257de18d
Mira Makadia <patelmirak@gmail.com>
2022-03-17 11:15:14 -0400

    adds CRUD for user review

--- main_app/views.py
+++ main_app/views.py
@@ -125,7 +125,7 @@ def point_of_interest_detail(request, point_of_interest_id):
     print(type(all_reviews))
     user_review = all_reviews.filter(user__exact=request.user.id)
     other_review = all_reviews.exclude(user__exact=request.user.id)
-
+    print("************",user_review)
     review_form=ReviewForm()
     if user_review:
         review_form = ReviewForm(instance = user_review.first())
